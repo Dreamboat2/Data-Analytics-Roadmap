bbc5848ffa1287c980b1d0bd7b69f6073f79c8f1
Dreamboat2 <126179934+Dreamboat2@users.noreply.github.com>
2024-12-15 16:04:08 -0800

    Add files via upload

---
+++ Select STATEMENT Tutourial.sql
@@ -0,0 +1,20 @@
+select*
+FROM parks_and_recreation.employee_demographics;
+
+
+select first_name,
+last_name, 
+birth_date,
+age,
+(age+10)*10
+FROM parks_and_recreation.employee_demographics;
+
+
+select Distinct first_name, gender
+FROM parks_and_recreation.employee_demographics;
+
+
+
+
+
+
